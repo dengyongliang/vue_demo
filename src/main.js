c7ef88d71e3d75aae7f9215b31924d243315675e
dyliang <dyliang1985@hotmail.com>
2019-07-04 12:36:01 +0800

    20190704

--- src/main.js
+++ src/main.js
@@ -3,13 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import store from './store'
+/* es6-shim,es6-promise ie兼容需要 */
+import 'es6-shim'
+require('es6-promise').polyfill()
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
---
+++ src/store/getters.js
@@ -0,0 +1,5 @@
+const getters = {
+  // tag: state => state.tags.tag,
+  // website: state => state.common.website
+}
+export default getters
